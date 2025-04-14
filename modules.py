946d90123d58498dad40f8e5a10a15f0fa0306c5
williamsadugyamfi <adugyamfiwilliams23@gmail.com>
2025-04-14 11:41:39 +0300

    Re enter password if it is weak

--- modules.py
+++ modules.py
@@ -16,21 +16,28 @@ if user_choice == "yes":
     print(f"Great! Your password ({auto_password}) is ready to use. Stay safe online")
 else:
     # Ask the user to input their own password
-    user_password = input("Enter your own password to check its strength: ")
+   # user_password = input("Enter your own password to check its strength: ")
 
     # Check password strength
-    length = len(user_password)
-    has_number = any(char.isdigit() for char in user_password)
-    has_upper = any(char.isupper() for char in user_password)
-    has_symbol = any(char in string.punctuation for char in user_password)
+    while True:
+         user_password = input("Enter your own password to check its strength: ")
+         length = len(user_password)
+         has_number = any(char.isdigit() for char in user_password)
+         has_upper = any(char.isupper() for char in user_password)
+         has_symbol = any(char in string.punctuation for char in user_password)
 
-    # Give feedback
-    print("Password Strength Check:")
-    if length < 8 or not (has_number or has_upper or has_symbol):
-        print(f"Your password ({user_password}) is weak. Try including numbers, symbols, and uppercase letters.")
-    elif 8 <= length <= 11 and sum([has_number, has_upper, has_symbol]) >= 2:
-        print(f"Your password ({user_password}) is moderate. Consider making it longer or more complex.")
-    elif length >= 12 and has_number and has_upper and has_symbol:
-        print(f"Your password ({user_password}) is strong. Great choice!")
-    else:
-        print(f"Your password ({user_password}) is acceptable but NOT Strong!.")
+   
+
+        # Give feedback
+        # print("Password Strength Check:")
+         if length < 8 or not (has_number or has_upper or has_symbol):
+            print(f"Your password ({user_password}) is weak. Try including numbers, symbols, and uppercase letters.")
+         elif 8 <= length <= 11 and sum([has_number, has_upper, has_symbol]) >= 2:
+            print(f"Your password ({user_password}) is moderate. Consider making it longer or more complex.")
+            break
+         elif length >= 12 and has_number and has_upper and has_symbol:
+            print(f"Your password ({user_password}) is strong. Great choice!")
+            break
+         else:
+            print(f"Your password ({user_password}) is acceptable but NOT Strong!.")
+            break
